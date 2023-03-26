7124aaea60121536dd2b4617af9bb78862827fb0
imranbinkalam <imranbinkalam123@gmail.com>
2023-03-27 01:50:13 +0600

    result

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Shop from "./Components/Shop/Shop";
 const App = () => {
   const [count, setCount] = useState(0);
   return (
     <div className="App">
       <Header></Header>
+      <Shop />
     </div>
   );
 };
