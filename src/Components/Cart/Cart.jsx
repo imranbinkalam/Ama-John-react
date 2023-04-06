d7a4d73a9f5a23acb1d2bfa9fdb52bc6bf3ed2f7
imranbinkalam <imranbinkalam123@gmail.com>
2023-04-06 23:04:53 +0600

    create

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Cart.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleClearCart, children }) => {
   // const cart = props.cart
   // const { cart } = props;
   // console.log(cart);
@@ -28,6 +30,11 @@ const Cart = ({ cart }) => {
       <p>Total shipping: ${totalShipping}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
       <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
+      <button onClick={handleClearCart} className="btn-clear">
+        <span>Clear Cart</span>
+        <FontAwesomeIcon icon={faTrashAlt} />
+      </button>
+      {children}
     </div>
   );
 };
